805b5515ccdf90c9d38b4917f07aeb3dd284b3d0
Morgan Estes <morgan.estes@gmail.com>
2016-11-02 12:08:35 -0500

    Added donut chart with ChartJS

--- index.js
+++ index.js
@@ -1,43 +1,132 @@
 (function(window, document, undefined) {
-    document.addEventListener('DOMContentLoaded', function() {
-        var ajaxButton;
-        var resultsEl;
-        var apiUrl;
-        var plugins = {};
-        var ajaxRequests = [];
-        var pluginSlugs = [
-            'coralcdn',
-            'custom-shipping-options-for-membermouse',
-            'remove-admin-bar-from-previews',
-            'widont-part-deux',
-            'windows-azure-storage',
-            'wp-hashgrid',
-        ];
+	document.addEventListener('DOMContentLoaded', function() {
+		var ajaxButton;
+		var resultsEl;
+		var apiUrl;
+		var plugins = {};
+		var ajaxRequests = [];
+		var pluginSlugs = [
+			'coralcdn',
+			'custom-shipping-options-for-membermouse',
+			'remove-admin-bar-from-previews',
+			'widont-part-deux',
+			'windows-azure-storage',
+			'wp-hashgrid',
+		];
 
-        console.log('ready');
-        ajaxButton = document.querySelector('#ajax-trigger');
-        resultsEl = document.querySelector('#data-results code');
+		console.log('ready');
+		ajaxButton = document.querySelector('#ajax-trigger');
+		resultsEl = document.querySelector('#data-results code');
 
-        ajaxButton.addEventListener('click', ajaxClicked, false);
-        //xhr.addEventListener('load', ajaxSave);
+		ajaxButton.addEventListener('click', ajaxClicked, false);
+		//xhr.addEventListener('load', ajaxSave);
 
-        function ajaxClicked() {
-            pluginSlugs.forEach(function(slug, index) {
-                console.debug('Getting data for ' + slug);
-                ajaxRequests[index] = new XMLHttpRequest();
-                ajaxRequests[index].open('GET', '/data/' +
-                    slug + '.json', true);
-                ajaxRequests[index].onload = function(e) {
-                    if (this.status === 200) {
-                        plugins[slug] = JSON.parse(
-                            ajaxRequests[index].responseText
-                        );
-                    }
-                };
-                ajaxRequests[index].send();
-            });
-            console.debug(plugins);
-        }
+		function ajaxClicked() {
+			pluginSlugs.forEach(function(slug, index) {
+				console.debug('Getting data for ' + slug);
+				ajaxRequests[index] = new XMLHttpRequest();
+				ajaxRequests[index].open('GET', '/data/' +
+					slug + '.json', true);
+				ajaxRequests[index].onload = function(e) {
+					if (this.status === 200) {
+						plugins[slug] = JSON.parse(
+							ajaxRequests[index].responseText
+						);
+					}
+				};
+				ajaxRequests[index].send();
+			});
+			console.debug(plugins);
+			console.log(pluginsCount().total);
+			pieChart();
+		}
 
-    }, false); //ready()
+		function pluginsCount() {
+			var totalDownloads = 0;
+
+			pluginSlugs.forEach(function(slug) {
+				if (plugins.hasOwnProperty(slug)) {
+					var p = plugins[slug];
+					console.debug('plugin data: %O', p);
+					//console.debug(typeof p);
+					if (p.downloaded) {
+						totalDownloads += parseInt(p.downloaded,
+							10);
+					}
+				}
+			});
+
+			return {
+				total: totalDownloads
+			};
+		}
+
+		function pieChart() {
+			// var data = {};
+			//
+			// pluginSlugs.forEach(function(slug) {
+			//     console.debug(plugins[slug]);
+			//     data[plugins[slug]] = parseInt(plugins[slug]
+			//         .downloaded, 10);
+			// });
+			//
+			// console.debug(data);
+			var getLabels = function getLabels() {
+				// yes, I know it's misspelled.
+				var lables = [];
+				pluginSlugs.forEach(function(slug) {
+					var plugin = plugins[slug];
+					if (typeof plugin !== 'undefined' && plugin.hasOwnProperty('name')) {
+						lables.push(plugins[slug].name);
+					}
+				});
+
+				return lables;
+			};
+			var getDownloads = function getDownloads() {
+				var downloads = [];
+				pluginSlugs.forEach(function(slug) {
+					var plugin = plugins[slug];
+					if (typeof plugin !== 'undefined' && plugin.hasOwnProperty(
+							'downloaded')) {
+						downloads.push(plugins[slug].downloaded);
+					}
+				});
+
+				return downloads;
+			};
+
+			var data = {
+				labels: getLabels(),
+				datasets: [{
+					data: getDownloads(),
+					backgroundColor: [
+						"#FF6384",
+						"#36A2EB",
+						"#FFCE56",
+						"#bac041",
+						"#c0ffee",
+						"#4a7092"
+					],
+					hoverBackgroundColor: [
+						"#FF6384",
+						"#36A2EB",
+						"#FFCE56",
+						"#bac041",
+						"#c0ffee",
+						"#4a7092"
+					]
+				}]
+			};
+			var ctx = document.querySelector('#pluginsPie');
+			var myDoughnutChart = new Chart(ctx, {
+				type: 'doughnut',
+				data: data,
+				options: {
+					fullWidth: false
+				}
+			});
+		}
+
+	}, false); //ready()
 })(window, document);
